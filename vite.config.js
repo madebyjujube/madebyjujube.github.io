9a9b30f65c803537e7ff07cd5d0e7740b43ba7e8
jujuu <67600283+madebyjujube@users.noreply.github.com>
2026-02-28 21:36:36 -0500

    debug-wrong-hashing

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,11 @@
 import { defineConfig } from 'vite'
 
 export default defineConfig({
+    root: '.',  // or 'src' if your source files are there
+    build: {
+        outDir: 'dist',
+        emptyOutDir: true,
+    },
     server: {
         port: 3000,
         proxy: {
@@ -10,8 +15,5 @@ export default defineConfig({
                 secure: false,
             }
         }
-    },
-    build: {
-        target: 'esnext'
     },
 })
